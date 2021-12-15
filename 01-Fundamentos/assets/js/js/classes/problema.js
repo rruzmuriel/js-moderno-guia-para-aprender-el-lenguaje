72b1b5bbc37e1a5ac8a7bcd1757217d7694be7a5
RAFAEL RUZ MURIEL <rruzmuriel@gmail.com>
2021-12-15 18:30:59 +0100

    Adding classes

---
+++ 01-Fundamentos/assets/js/js/classes/problema.js
@@ -0,0 +1,37 @@
+
+
+const rruz = {
+    nombre: 'Rafa',
+    edad: 30,
+    imprimir() {
+        console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);    
+    }
+}
+
+const pedro = {
+    nombre: 'Pedro',
+    edad: 15,
+    imprimir() {
+        console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);    
+    }
+}
+
+// rruz.imprimir();
+// Ok esto se debe de crear con la palabra new
+function Persona(nombre, edad) {
+    console.log('Se ejecutó esta línea');
+
+    this.nombre = nombre;
+    this.edad   = edad;   
+
+    this.imprimir = function() {
+        console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);    
+    }
+
+}
+
+const maria   = new Persona('María', 18);
+const melissa = new Persona('Melissa', 35);
+// console.log( maria );
+maria.imprimir();
+melissa.imprimir();
