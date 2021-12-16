df9246386f6dbd5a201a858fb32bd3144b007831
RAFAEL RUZ MURIEL <rruzmuriel@gmail.com>
2021-12-16 17:03:46 +0100

    Initializing webpack installation

---
+++ 03-webpack-inicial/src/index.js
@@ -0,0 +1,4 @@
+import { saludar } from "./js/componentes";
+
+const nombre = 'Rafael';
+saludar(nombre);
---
+++ 03-webpack-inicial/src/js/componentes.js
@@ -0,0 +1,9 @@
+
+export const saludar = (nombre)=>{
+    console.log('Creando etiqueta 1');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola ${nombre}`;
+
+    document.body.append(h1)
+};
