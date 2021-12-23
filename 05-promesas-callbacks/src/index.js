a3578a525820b101a8ddf2dbbf439efcef9102cb
RAFAEL RUZ MURIEL <rruzmuriel@gmail.com>
2021-12-23 17:43:40 +0100

    Adding promise all

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -19,6 +19,22 @@ const heroeId2 = 'iron';
 
 // });
 
-buscarHeroe(heroeId1).then(heroe => {
-    console.log(`Enviando a ${heroe.nombre} a la misión`);
-})
+// buscarHeroe(heroeId1).then(heroe1 => {
+//     console.log(`Enviando a ${heroe.nombre} a la misión`);
+//     buscarHeroe(heroeId2).then(heroe2 => {
+//         console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión `)
+
+//     });
+// });
+
+
+// Como funciona promise all
+// Promise.all([true, 'hola', 123]).then( arr => {
+
+//     console.log('promise.all', arr);
+// });
+
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)])
+        .then(([heroe1, heroe2]) => {
+    console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión `);    
+});
