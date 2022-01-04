fb10a2cd2192a8b47a5ec3203199e71cdaea561f
RAFAEL RUZ MURIEL <rruzmuriel@gmail.com>
2022-01-04 12:20:34 +0100

    Adding file http

--- 06-http/src/js/http-provider.js
+++ 06-http/src/js/http-provider.js
@@ -1,6 +1,10 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 const urlUsuarios ='https://reqres.in/api/users?page=2';
 
+// Cloudinary
+const cloudPreset = 'lace5ckd';
+const cloudUrl    = 'https://api.cloudinary.com/v1_1/dxyngl2sd/upload';
+
 const obtenerChiste = async()=> {
 
     try {
@@ -27,7 +31,36 @@ const obtenerUsuarios = async()=> {
     const{data: usuario} = await resp.json();
     return usuario;
 }
+
+// ArchivoSubir :: File
+const subirImagen = async( archivoSubir ) => {
+
+    const formData = new FormData();
+    formData.append('upload_preset', cloudPreset );
+    formData.append('file', archivoSubir );
+
+    try { 
+        
+        const resp = await fetch( cloudUrl, {
+            method: 'POST',
+            body: formData
+        });
+
+        if ( resp.ok ) {
+            const cloudResp = await resp.json();
+            return cloudResp.secure_url; 
+        } else {
+            throw await resp.json();
+        }
+
+    } catch( err ) {
+        throw err;
+    }
+
+}
+
 export {
     obtenerChiste,
-    obtenerUsuarios
+    obtenerUsuarios,
+    subirImagen
 }
